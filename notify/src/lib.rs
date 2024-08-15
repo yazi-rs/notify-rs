ce60ae911dc2d889ca7e470af62d9088106cb09f
sxyazi <sxyazi@gmail.com>
2024-08-15 22:16:47 +0800

    Publish

--- notify/src/lib.rs
+++ notify/src/lib.rs
@@ -165,7 +165,7 @@
 
 pub use config::{Config, RecursiveMode};
 pub use error::{Error, ErrorKind, Result};
-pub use notify_types::event::{self, Event, EventKind};
+pub use notify_types_fork::event::{self, Event, EventKind};
 use std::path::Path;
 
 pub(crate) type Receiver<T> = std::sync::mpsc::Receiver<T>;
